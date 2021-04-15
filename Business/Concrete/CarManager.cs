d0384ce0886a406b43c3cc45a273a11585ae301a
SkymanSky <gokera@ogr.iu.edu.tr>
2021-04-16 00:23:06 +0300

    AOP Support Added.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FulentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -48,11 +49,9 @@ namespace Business.Concrete
             return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(),Messages.CarListed);
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            
-
-            ValidationTool.Validate(new CarValidator(),car);
             _carDal.Add(car);
             return new SuccessResult(Messages.CarAdded);
         }
